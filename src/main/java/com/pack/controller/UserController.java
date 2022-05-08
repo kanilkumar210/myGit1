9d8e55b7e1750b3de9465f0d711d2f1cf4d8741a
kanilkumar210 <anilkarre8@gmail.com>
2022-05-08 13:22:45 +0530

    second commit

--- src/main/java/com/pack/controller/UserController.java
+++ src/main/java/com/pack/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.pack.controller;
 
-import java.util.NoSuchElementException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +14,7 @@ import com.pack.model.User;
 import com.pack.service.CorporateService;
 import com.pack.service.UserService;
 
+//Anil
 @Controller
 public class UserController {
 
